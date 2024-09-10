dc61158ec21741e213345be80309507e9923560e
aryajava <muhammad.aryajava@gmail.com>
2024-09-10 12:29:07 +0700

    rev. 01

--- js-challenges/challenge01.js
+++ js-challenges/challenge01.js
@@ -1,7 +1,12 @@
 function sum() {
     let val = 0
-    for (i of arguments) {
+    for (let i of arguments) {
         val +=i
     }
     return console.log(val)
 }
+
+sum(1,2,7) // 10
+sum(1,4) // 5
+sum(11) // 11
+sum(10,3,6,7,9) // 35
