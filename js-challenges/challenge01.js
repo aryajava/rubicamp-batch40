182bdf6de616f665c596ac7f7872decd91a2b791
aryajava <muhammad.aryajava@gmail.com>
2024-09-10 12:26:59 +0700

    rev. 01

--- js-challenges/challenge01.js
+++ js-challenges/challenge01.js
@@ -1,14 +1,7 @@
-// function sum() {
-//     let val = 0
-//     for (i of arguments) {
-//         val +=i
-//     }
-//     return console.log(val)
-// }
-
-const sum = (...args) => args.reduce((a, b) => a + b)
-
-console.log(sum(1,2,7)) // 10
-console.log(sum(1,4)) // 5
-console.log(sum(11)) // 11
-console.log(sum(10,3,6,7,9)) // 35
+function sum() {
+    let val = 0
+    for (i of arguments) {
+        val +=i
+    }
+    return console.log(val)
+}
