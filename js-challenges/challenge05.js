823c7d5299878043bddd65c58744beefc91134f1
aryajava <muhammad.aryajava@gmail.com>
2024-09-10 13:23:29 +0700

    rev. 02

--- js-challenges/challenge05.js
+++ js-challenges/challenge05.js
@@ -1,7 +1,7 @@
 function stringManipulation(word) {
     const vowel_letters = ['a', 'i', 'u', 'e', 'o'];
-    return vowel_letters.includes(word[0])? word : word.slice(1) + word[0] + 'nyo';
+    return vowel_letters.includes(word[0].toLowerCase())? word : word.slice(1) + word[0] + 'nyo';
 }
 
-console.log(stringManipulation('ayam'));
-console.log(stringManipulation('bebek'));
+console.log(stringManipulation('AYAM'));
+console.log(stringManipulation('Bebek'));
