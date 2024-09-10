c66b8291cf476dda747b2b053216916a79e4f350
aryajava <muhammad.aryajava@gmail.com>
2024-09-10 13:30:46 +0700

    rev. 01

--- js-challenges/challenge06.js
+++ js-challenges/challenge06.js
@@ -1,12 +1,11 @@
 function sentencesManipulation(sentence) {
-    const vowel_letters = ['a', 'i', 'u', 'e', 'o']
+    const vowel_letters = ['a', 'i', 'u', 'e', 'o'];
     let words = sentence.split(' ');
     let proc_words = words.map(word => {
-        return vowel_letters.includes(word[0])?  word : word.slice(1) + word[0] + 'nyo'
+        return vowel_letters.includes(word[0].toLowerCase())? word : word.slice(1) + word[0] + 'nyo';
     })
-    return proc_words.join(' ')
+    
+    return proc_words.join(' ');
 }
 
-export {sentencesManipulation}
-
-// console.log(sentencesManipulation('ibu pergi ke pasar bersama aku')) // ibu ergipnyo eknyo asarpnyo ersamabnyo aku
+console.log(sentencesManipulation('ibu pergi ke pasar bersama aku'));
