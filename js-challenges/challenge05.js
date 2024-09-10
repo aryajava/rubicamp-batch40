2a0eb5af4966468a3e8b4bb62ff50fb717ddd3e6
aryajava <muhammad.aryajava@gmail.com>
2024-09-10 13:12:34 +0700

    rev. 01

--- js-challenges/challenge05.js
+++ js-challenges/challenge05.js
@@ -1,20 +1,7 @@
 function stringManipulation(word) {
-    vowel_letters = ['a', 'i', 'u', 'e', 'o']
-    // Menggunakan 'includes'
-    return vowel_letters.includes(word[0])?  word : word.slice(1) + word[0] + 'nyo'
-
-    // Menggunakan nested loop 'for'
-    // for (let i = 0; i <= word.length; i++) {
-    //     for (let j = 0; j < vowel_letters.length; j++) {
-    //         if (word[0] == vowel_letters[j]){
-    //             return word
-    //         }
-    //         else{
-    //             return word.slice(1) + word[0] + 'nyo'
-    //         }
-    //     }
-    // }
+    const vowel_letters = ['a', 'i', 'u', 'e', 'o'];
+    return vowel_letters.includes(word[0])? word : word.slice(1) + word[0] + 'nyo';
 }
 
-console.log(stringManipulation('ayam')); // ayam
-console.log(stringManipulation('bebek')); // ebekbnyo
+console.log(stringManipulation('ayam'));
+console.log(stringManipulation('bebek'));
