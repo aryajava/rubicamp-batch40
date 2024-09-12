3486e10a29667d7e61df5acfd664fab85a2812db
aryajava <muhammad.aryajava@gmail.com>
2024-09-12 16:31:51 +0700

    rev. 01

--- js-challenges/challenge12.js
+++ js-challenges/challenge12.js
@@ -1,85 +1,89 @@
-import readline from "readline"
-import fs from "fs"
+import readline from "readline";
+import fs from "fs";
 
-// Membuat argument
-const args = process.argv.slice(2)
+// Mengambil argument dari command line
+const arg = process.argv.slice(2, 3);
+const lenArg = arg.length;
 
-// Membaca input
+if (lenArg === 0) {
+    console.log(`Tolong sertakan nama file sebagai inputan soalnya.\nMisalnya 'node input.js data.json'.`);
+    process.exit();
+}
+
+// Membuat interface readline
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
-// Fungsi validasi soal
-function validasiSoal(x) {
-    return Array.isArray(x) && x.every(key => typeof key.definition === 'string' && typeof key.term === 'string')
-}
+// Variabel
+let index = 0;
+let wAnsw = 0;
+let skippedQuestions = [];
+let data = [];
 
-// Validasi apakah terdapat argument
-if (args.length > 0) {
-    const filename = args[0]
-    // Validasi apakah arg tersebut file soal
+// Fungsi untuk menampilkan pertanyaan
+const _pertanyaan = () => {
+    if (index < data.length) {
+        console.log(`\nPertanyaan: ${data[index].definition}`);
+        // process.stdout.write('Tebakan: ');
+        rl.setPrompt('Tebakan: ');
+        rl.prompt();
+    } else if (skippedQuestions.length > 0) {
+        data = skippedQuestions.slice();
+        index = 0;
+        wAnsw = 0;
+        skippedQuestions = [];
+        _pertanyaan();
+    } else {
+        console.log("\nAnda Berhasil!");
+        rl.close();
+    }
+};
+
+// Fungsi untuk memproses jawaban
+const _jawaban = (answer) => {
+    if (answer.toLowerCase() === 'skip') {
+        skippedQuestions.push(data[index]);
+        wAnsw = 0;
+        index++;
+        _pertanyaan();
+    } else if (answer.toLowerCase() === data[index].term.toLowerCase()) {
+        console.log('\nAnda Beruntung!');
+        wAnsw = 0;
+        index++;
+        _pertanyaan();
+    } else {
+        wAnsw++;
+        console.log(`\nAnda kurang beruntung! Anda telah salah ${wAnsw} kali, silakan coba lagi.`);
+        // process.stdout.write('Tebakan: ');
+        rl.setPrompt('Tebakan: ');
+        rl.prompt();
+    }
+};
+
+// Fungsi utama untuk menjalankan permainan
+const gameTebakKata = () => {
     try {
-        let data = JSON.parse(fs.readFileSync(filename, "utf-8"))
-        if (validasiSoal(data)) {
-            let wAnsw = 0, index = 0
-            const skippedQuestions = []
-            console.log(`Selamat Datang di permainan 'Tebak-tebakan'. Kamu akan diberikan pertanyaan dari file ini '${filename}'.\nUntuk bermain, jawablah dengan jawaban yang sesuai.`)
-
-            function askQuestion() {
-                // Validasi jumlah pertanyaan
-                if (index < data.length) {
-                    // Mulai pertanyaan
-                    console.log(`\nPertanyaan: ${capitalizeFirstLetter(data[index].definition)}`)
-                    getAnswer()
-                } else if (skippedQuestions.length > 0) {
-                    // Jika ada pertanyaan yang diskip, tanyakan ulang
-                    data = skippedQuestions.slice()
-                    // Reset variabel
-                    index = 0
-                    wAnsw = 0
-                    skippedQuestions.length = 0
-                    askQuestion() // Lanjut pertanyaan yang diulang
-                } else {
-                    // Jika tidak ada lagi pertanyaan
-                    console.log("Anda berhasil!")
-                    rl.close()
-                }
-            }
-
-            function getAnswer() {
-                rl.question('Tebakan: ', answer => {
-                    if (answer.toLowerCase() === 'skip') {
-                        // Simpan index pertanyaan
-                        skippedQuestions.push(data[index])
-                        index++
-                        askQuestion() // Lanjut pertanyaan berikutnya
-                    } else if (answer.toLowerCase() === data[index].term.toLowerCase()) {
-                        console.log('\nAnda beruntung!')
-                        index++
-                        askQuestion() // Lanjut pertanyaan berikutnya
-                    } else {
-                        wAnsw++ // Tambah Total tebakan salah
-                        console.log(`\nAnda kurang beruntung! Anda telah salah ${wAnsw} kali, silahkan coba lagi.`)
-                        getAnswer() // Ulangi input Tebakan
-                    }
-                })
-            }
-
-            // Fungsi membuat huruf kapital
-            function capitalizeFirstLetter(string) {
-                return string.charAt(0).toUpperCase() + string.slice(1)
-            }
-            askQuestion()
+        data = JSON.parse(fs.readFileSync(arg[0], "utf-8"));
+        const lenData = data.length;
+        const soalValid = (x) => {
+            return Array.isArray(x) && x.every(key => typeof key.definition === 'string' && typeof key.term === 'string');
+        };
+        if (lenData !== 0 && soalValid(data)) {
+            console.log(`Selamat Datang di permainan 'Tebak-tebakan'. Kamu akan diberikan pertanyaan dari file '${arg[0]}'.\nUntuk bermain, jawablah dengan jawaban yang sesuai.`);
+            _pertanyaan();
+            rl.on('line', (answer) => {
+                _jawaban(answer);
+            });
         } else {
-            console.error('File tidak valid.')
-            process.exit(1)
+            console.log(`Soal '${arg[0]}' tidak valid.`);
+            rl.close();
         }
     } catch (err) {
-        console.error('Gagal membaca file soal, file: ', err)
-        process.exit(1)
+        console.error(`Gagal membaca file soal: ${err.message}`);
+        rl.close();
     }
-} else {
-    console.log(`Tolong sertakan nama file sebagai inputan soalnya.\nMisalnya 'node input.js soal.json'.`)
-    process.exit(1)
-}
+};
+
+gameTebakKata();
