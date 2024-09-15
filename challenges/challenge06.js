5a937607cddf423fda5b9153756a14ede37471ec
aryajava <muhammad.aryajava@gmail.com>
2024-09-15 15:32:54 +0700

    add varian

--- challenges/challenge06.js
+++ challenges/challenge06.js
@@ -1,11 +1,17 @@
 function sentencesManipulation(sentence) {
     const vowel_letters = ['a', 'i', 'u', 'e', 'o'];
-    let words = sentence.split(' ');
-    let proc_words = words.map(word => {
-        return vowel_letters.includes(word[0].toLowerCase())? word : word.slice(1) + word[0] + 'nyo';
-    })
-    
-    return proc_words.join(' ');
+    const words = sentence.split(' ');
+    const result = [];
+    for (let i = 0; i < words.length; i++) {
+        result.push(vowel_letters.includes(words[i][0]) ? words[i] : words[i].slice(1) + words[i][0] + 'nyo');
+    }
+    console.log(result.join(' '));
+
+    // let proc_words = words.map(word => {
+    //     return vowel_letters.includes(word[0].toLowerCase()) ? word : word.slice(1) + word[0] + 'nyo';
+    // })
+
+    // console.log(proc_words.join(' '));
 }
 
-console.log(sentencesManipulation('ibu pergi ke pasar bersama aku'));
+sentencesManipulation('ibu pergi ke pasar bersama aku');
