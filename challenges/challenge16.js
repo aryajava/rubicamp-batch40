64f8464ee13e523ca800734e004a6cc5074772a9
aryajava <muhammad.aryajava@gmail.com>
2024-09-18 14:38:01 +0700

    rev. 01

--- challenges/challenge16.js
+++ challenges/challenge16.js
@@ -1,26 +1,26 @@
 // npm install uuid
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 class Tyre {
     constructor(brand, size) {
-        this.brand = brand
-        this.size = size
+        this.brand = brand;
+        this.size = size;
     }
 }
 
 class Car {
     constructor(model, year, seats, doors, warranty) {
-        this.model = model
-        this.year = year
-        this.sn = uuidv4()
-        this.tyres = []
-        this.seats = seats
-        this.doors = doors
-        this.warranty = warranty
+        this.model = model;
+        this.year = year;
+        this.sn = uuidv4();
+        this.tyres = [];
+        this.seats = seats;
+        this.doors = doors;
+        this.warranty = warranty;
     }
 
     addTyre(tyre) {
-        this.tyres.push(tyre)
+        this.tyres.push(tyre);
     }
 
     getDetails() {
@@ -32,78 +32,112 @@ class Car {
             seats: this.seats,
             doors: this.doors,
             warranty: `${this.warranty} year`,
-        }
+        };
     }
 
     isWarrantyActive(checkYear) {
-        return (this.year + this.warranty) >= checkYear
+        return this.year + this.warranty >= checkYear;
     }
 }
 
 class CarFactory {
     constructor() {
-        this.cars = []
+        this.cars = [];
     }
 
     produce(year) {
-        const targetCount = year === 2020 ? 5 : 7
+        let agyaCount, rushCount;
+        if (year === 2020) {
+            agyaCount = 4;
+            rushCount = 1;
+        } else if (year === 2022) {
+            agyaCount = 3;
+            rushCount = 4;
+        } else {
+            console.log("Masukan tahun yang sesuai");
+            process.exit();
+        }
+
         const models = [
-            // Tambahkan varian, door, seat, brand ban, size ban, dan warranty
-            { name: 'Agya', doors: 5, seats: 5, tyreBrand: 'dunlop', tyreSize: '15 inch', warranty: 1 },
-            { name: 'Rush', doors: 5, seats: 5, tyreBrand: 'Bridgestone', tyreSize: '17 inch', warranty: 3 },
-        ]
+            { name: "Agya", doors: 5, seats: 5, tyreBrand: "dunlop", tyreSize: "15 inch", warranty: 1 },
+            {
+                name: "Rush",
+                doors: 5,
+                seats: 5,
+                tyreBrand: "Bridgestone",
+                tyreSize: "17 inch",
+                warranty: 3,
+            },
+        ];
 
-        for (let i = 0; i < targetCount; i++) {
-            const randomModel = models[Math.floor(Math.random() * models.length)]
-            const car = new Car(randomModel.name, year, randomModel.seats, randomModel.doors, randomModel.warranty)
+        // Produksi Agya
+        for (let i = 0; i < agyaCount; i++) {
+            const car = new Car(
+                models[0].name,
+                year,
+                models[0].seats,
+                models[0].doors,
+                models[0].warranty
+            );
+            car.addTyre(new Tyre(models[0].tyreBrand, models[0].tyreSize));
+            this.cars.push(car);
+        }
 
-            car.addTyre(new Tyre(randomModel.tyreBrand, randomModel.tyreSize))
-            this.cars.push(car)
+        // Produksi Rush
+        for (let i = 0; i < rushCount; i++) {
+            const car = new Car(
+                models[1].name,
+                year,
+                models[1].seats,
+                models[1].doors,
+                models[1].warranty
+            );
+            car.addTyre(new Tyre(models[1].tyreBrand, models[1].tyreSize));
+            this.cars.push(car);
         }
     }
 
     result() {
-        console.log(`Hasil Produksi: \n`)
+        console.log(`Hasil Produksi: \n`);
         this.cars.forEach((car, index) => {
-            const details = car.getDetails()
-            console.log(`no. ${index + 1}`)
-            console.log(`varian     : ${details.model}`)
-            console.log(`sn         : ${details.sn}`)
-            console.log(`door       : ${details.doors}`)
-            console.log(`seat       : ${details.seats} seater`)
-            console.log(`tyre       : ${details.tyres}`)
-            console.log(`year       : ${details.year}`)
-            console.log(`warranty   : ${details.warranty}`)
-            console.log('-----')
-        })
+            const details = car.getDetails();
+            console.log(`no. ${index + 1}`);
+            console.log(`varian     : ${details.model}`);
+            console.log(`sn         : ${details.sn}`);
+            console.log(`door       : ${details.doors}`);
+            console.log(`seat       : ${details.seats} seater`);
+            console.log(`tyre       : ${details.tyres}`);
+            console.log(`year       : ${details.year}`);
+            console.log(`warranty   : ${details.warranty}`);
+            console.log("\n");
+        });
     }
 
     guaranteeSimulation(simulationYear) {
-        console.log(`\nHasil Simulasi Garansi Semua Mobil Pada Tahun ${simulationYear}: \n`)
+        console.log(`Hasil Simulasi Garansi Semua Mobil Pada Tahun ${simulationYear}: \n`);
         this.cars.forEach((car, index) => {
-            const details = car.getDetails()
-            console.log(`no. ${index + 1}`)
-            console.log(`varian     : ${details.model}`)
-            console.log(`sn         : ${details.sn}`)
-            console.log(`door       : ${details.doors}`)
-            console.log(`seat       : ${details.seats} seater`)
-            console.log(`tyre       : ${details.tyres}`)
-            console.log(`year       : ${details.year}`)
-            console.log(`warranty   : ${details.warranty}`)
-            
+            const details = car.getDetails();
+            console.log(`no. ${index + 1}`);
+            console.log(`varian     : ${details.model}`);
+            console.log(`sn         : ${details.sn}`);
+            console.log(`door       : ${details.doors}`);
+            console.log(`seat       : ${details.seats} seater`);
+            console.log(`tyre       : ${details.tyres}`);
+            console.log(`year       : ${details.year}`);
+            console.log(`warranty   : ${details.warranty}`);
+
             if (car.isWarrantyActive(simulationYear)) {
-                console.log(`status on ${simulationYear} this guarantee status active`)
+                console.log(`\nstatus on ${simulationYear} this guarantee status active`);
             } else {
-                console.log(`status on ${simulationYear} this guarantee status expired`)
+                console.log(`\nstatus on ${simulationYear} this guarantee status expired`);
             }
-            console.log('-----')
-        })
+            console.log("");
+        });
     }
 }
 
-// Contoh Penggunaan
-const toyota = new CarFactory()
-toyota.produce(2020)
-toyota.produce(2022)
-toyota.result()
-toyota.guaranteeSimulation(2025)
+const toyota = new CarFactory();
+toyota.produce(2020);
+toyota.produce(2022);
+toyota.result();
+toyota.guaranteeSimulation(2025);
