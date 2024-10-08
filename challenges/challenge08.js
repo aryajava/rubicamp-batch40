f109265f5423a807a15970f7546800e0c3ddd713
aryajava <muhammad.aryajava@gmail.com>
2024-10-08 11:01:28 +0700

    Update challenge08.js

--- challenges/challenge08.js
+++ challenges/challenge08.js
@@ -1,13 +1,13 @@
 function pola(str) {
-    let [part1, part2] = str.split('=');
-    for (let i = 0; i < 10; i++) {
-        let rePart1 = part1.replace('#', i);
-        for (let j = 0; j < 10; j++) {
-            let rePart2 = part2.replace('#', j);
-            if (eval(rePart1) == rePart2) return [i, j];
-        };
-    };
-};
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      if (eval(str.replace("#", i).replace("#", j).replace("=", "=="))) {
+        return [i, j];
+      }
+    }
+  }
+}
 
 console.log(pola("42#3 * 188 = 80#204")); // result: [8, 5]
 console.log(pola("8#61 * 895 = 78410#5")); // result: [7, 9]
+
